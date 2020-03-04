9f0f9920f544d8052df54a3a40677d9c731b643b
ewashchysion <ewashchysion@14west.us>
2020-03-04 16:42:53 -0500

    initial project

--- hackteam5/src/main/java/com/agora/hackteam5/SampleController.java
+++ hackteam5/src/main/java/com/agora/hackteam5/SampleController.java
@@ -1,4 +1,4 @@
-package com.example.hackteam5;
+package com.agora.hackteam5;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- hackteam5/src/test/java/com/agora/hackteam5/Hackteam5ApplicationTests.java
+++ hackteam5/src/test/java/com/agora/hackteam5/Hackteam5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hackteam5;
+package com.agora.hackteam5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
