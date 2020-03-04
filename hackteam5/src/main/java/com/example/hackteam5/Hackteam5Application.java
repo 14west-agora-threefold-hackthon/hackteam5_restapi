34866bcd71b938fe77822330c8e4d1a273dd7ff3
ewashchysion <ewashchysion@14west.us>
2020-03-04 15:52:01 -0500

    initial project

---
+++ hackteam5/src/main/java/com/example/hackteam5/Hackteam5Application.java
@@ -0,0 +1,13 @@
+package com.example.hackteam5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackteam5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackteam5Application.class, args);
+	}
+
+}
---
+++ hackteam5/src/test/java/com/example/hackteam5/Hackteam5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackteam5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackteam5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
